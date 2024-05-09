0e38548868c7d743631028459ca2360854bfecb0
VladislavNuriev <rubis3@ya.ru>
2024-05-09 09:07:54 +0200

    Some corrections

--- app/src/main/java/com/example/shoppinglist/domain/EditShopItemUseCase.kt
+++ app/src/main/java/com/example/shoppinglist/domain/EditShopItemUseCase.kt
@@ -2,6 +2,6 @@ package com.example.shoppinglist.domain
 
 class EditShopItemUseCase(private val shopListRepository: ShopListRepository)   {
     fun editeShopItem(shopItem: ShopItem){
-        shopListRepository.editeShopItem(shopItem)
+        shopListRepository.editShopItem(shopItem)
     }
 }
--- app/src/main/java/com/example/shoppinglist/domain/ShopListRepository.kt
+++ app/src/main/java/com/example/shoppinglist/domain/ShopListRepository.kt
@@ -3,7 +3,7 @@ package com.example.shoppinglist.domain
 interface ShopListRepository {
     fun addShopItem(shopItem: ShopItem)
     fun getShopItem(shopItemId: Int): ShopItem
-    fun editeShopItem(shopItem: ShopItem)
+    fun editShopItem(shopItem: ShopItem)
     fun getShopList(): List<ShopItem>
     fun deleteShopItem(id: Int)
 }
