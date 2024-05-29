7febe3404e54a049dbb0dbab3e90a4c434cc680c
VladislavNuriev <rubis3@ya.ru>
2024-05-29 21:00:20 +0200

    Optimized ShoppingListAdapter (implemented ListAdapter with ShopItemDiffCallback, extracted ShopItemViewHolder to a separate class.

--- app/src/main/java/com/example/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglist/presentation/MainActivity.kt
@@ -29,7 +29,7 @@ class MainActivity : AppCompatActivity() {
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
         setupRecyclerView()
         viewModel.shopList.observe(this) {
-            shopListAdapter.shopList = it
+            shopListAdapter.submitList(it)
         }
 
     }
@@ -67,7 +67,7 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
-                val item = shopListAdapter.shopList[viewHolder.adapterPosition]
+                val item = shopListAdapter.currentList[viewHolder.adapterPosition]
                 viewModel.deleteShopItem(item)
             }
         }
--- app/src/main/java/com/example/shoppinglist/presentation/ShoppingListAdapter.kt
+++ app/src/main/java/com/example/shoppinglist/presentation/ShoppingListAdapter.kt
@@ -1,51 +1,29 @@
 package com.example.shoppinglist.presentation
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.recyclerview.widget.DiffUtil
-import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.ListAdapter
 import com.example.shoppinglist.R
 import com.example.shoppinglist.domain.ShopItem
 
-class ShoppingListAdapter() :
-    RecyclerView.Adapter<ShoppingListAdapter.ViewHolder>() {
+class ShoppingListAdapter :
+    ListAdapter<ShopItem, ShopItemViewHolder>(ShopItemDiffCallback()) {
 
     /**
      * Provide a reference to the type of views that you are using
      * (custom ViewHolder)
      */
-    companion object{
+    companion object {
         const val VIEW_TYPE_ENABLED = 100
         const val VIEW_TYPE_DISABLED = 101
         const val MAX_POOL_SIZE = 15
     }
 
-    var shopList = listOf<ShopItem>()
-        set(value) {
-            val callback = ShopListDiffCallback(shopList, value)
-            val diffResult = DiffUtil.calculateDiff(callback)
-            diffResult.dispatchUpdatesTo(this)
-            field = value
-        }
-
     var onShopItemLongClickListener: ((ShopItem) -> Unit)? = null
     var onShopItemClickListener: ((ShopItem) -> Unit)? = null
 
-    class ViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
-        val textViewName: TextView
-        val textViewCount: TextView
-
-        init {
-            // Define click listener for the ViewHolder's View
-            textViewCount = view.findViewById(R.id.tv_count)
-            textViewName = view.findViewById(R.id.tv_name)
-        }
-    }
-
     // Create new views (invoked by the layout manager)
-    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ShopItemViewHolder {
         // Create a new view, which defines the UI of the list item
         val layout = when (viewType) {
             VIEW_TYPE_ENABLED -> R.layout.item_shop_enabled
@@ -53,17 +31,17 @@ class ShoppingListAdapter() :
             else -> throw RuntimeException("Unknown view type: $viewType")
         }
         val view = LayoutInflater.from(viewGroup.context)
-                .inflate(layout, viewGroup, false)
-        return ViewHolder(view)
+            .inflate(layout, viewGroup, false)
+        return ShopItemViewHolder(view)
     }
 
     // Replace the contents of a view (invoked by the layout manager)
-    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+    override fun onBindViewHolder(shopItemViewHolder: ShopItemViewHolder, position: Int) {
 
         // Get element from your dataset at this position and replace the
         // contents of the view with that element
-        val shopItem = shopList[position]
-        with(viewHolder) {
+        val shopItem = getItem(position)
+        with(shopItemViewHolder) {
             textViewName.text = shopItem.name
             textViewCount.text = shopItem.count.toString()
             view.setOnLongClickListener {
@@ -74,13 +52,9 @@ class ShoppingListAdapter() :
         }
     }
 
-    // Return the size of your dataset (invoked by the layout manager)
-    override fun getItemCount() = shopList.size
-
     override fun getItemViewType(position: Int): Int {
-        return if (shopList[position].enabled)
+        return if (getItem(position).enabled)
             VIEW_TYPE_ENABLED
         else VIEW_TYPE_DISABLED
     }
-
 }
