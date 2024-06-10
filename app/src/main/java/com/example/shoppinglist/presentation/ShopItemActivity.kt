47f01d367cf4e0b69fa7b4dbb4b2387b390e08e8
VladislavNuriev <rubis3@ya.ru>
2024-06-10 20:29:06 +0200

    ShopItemFragment created

--- app/src/main/java/com/example/shoppinglist/presentation/ShopItemActivity.kt
+++ app/src/main/java/com/example/shoppinglist/presentation/ShopItemActivity.kt
@@ -3,21 +3,13 @@ package com.example.shoppinglist.presentation
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.core.widget.doOnTextChanged
-import androidx.lifecycle.ViewModelProvider
 import com.example.shoppinglist.R
-import com.example.shoppinglist.databinding.ActivityShopItemBinding
 import com.example.shoppinglist.domain.ShopItem
 
-class ShopItemActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityShopItemBinding
-    private lateinit var viewModel: ShopItemViewModel
+class ShopItemActivity : AppCompatActivity() {
 
     private var screenMode = MODE_UNKNOWN
     private var shopItemId = ShopItem.UNDEFINED_ID
@@ -25,20 +17,8 @@ class ShopItemActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-
-        binding = ActivityShopItemBinding.inflate(layoutInflater)
-        val view: View = binding.getRoot()
-        setContentView(view)
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        setContentView(R.layout.activity_shop_item)
         parseIntent()
-        viewModel = ViewModelProvider(this)[ShopItemViewModel::class.java]
-        observeViewModel()
-        addTextChangeListeners()
         launchRightMode()
     }
 
@@ -60,60 +40,14 @@ class ShopItemActivity : AppCompatActivity() {
     }
 
     private fun launchRightMode() {
-        when (screenMode) {
-            MODE_EDIT -> launchEditMode()
-            MODE_ADD -> launchAddMode()
+        val fragment = when (screenMode) {
+            MODE_EDIT -> ShopItemFragment.newInstanceEditMode(shopItemId)
+            MODE_ADD -> ShopItemFragment.newInstanceAddItem()
+            else -> throw RuntimeException("Screen mode undefined")
         }
-    }
-
-    private fun launchEditMode() {
-        viewModel.getShopItem(shopItemId)
-        viewModel.shopItem.observe(this@ShopItemActivity) {
-            binding.editTextName.setText(it.name)
-            binding.editTextCount.setText(it.count.toString())
-        }
-        binding.buttonSave.setOnClickListener {
-            viewModel.editShopItem(
-                binding.editTextName.text.toString(),
-                binding.editTextCount.text.toString()
-            )
-        }
-    }
-
-    private fun launchAddMode() {
-        binding.buttonSave.setOnClickListener {
-            viewModel.addShopItem(
-                binding.editTextName.text.toString(),
-                binding.editTextCount.text.toString()
-            )
-        }
-    }
-
-    private fun observeViewModel() {
-        viewModel.shouldCloseScreen.observe(this@ShopItemActivity) {
-            finish()
-        }
-        viewModel.errorInputName.observe(this@ShopItemActivity) {
-            if (it) {
-                binding.textInputName.error = "Insert item"
-            }
-        }
-        viewModel.errorInputCount.observe(this@ShopItemActivity) {
-            if (it) {
-                binding.textInputCount.error = "Must be > 0"
-            }
-        }
-    }
-
-    private fun addTextChangeListeners() {
-        binding.editTextName.doOnTextChanged { text, start, before, count ->
-            binding.textInputName.error = null
-            viewModel.resetErrorInputName()
-        }
-        binding.editTextCount.doOnTextChanged { text, start, before, count ->
-            binding.textInputCount.error = null
-            viewModel.resetErrorInputCount()
-        }
+        supportFragmentManager.beginTransaction()
+            .add(R.id.shop_item_container, fragment)
+            .commit()
     }
 
     companion object {
